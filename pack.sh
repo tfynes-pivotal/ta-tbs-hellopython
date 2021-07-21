004b2e3d6d013fd10b0dbfc43d7a4cdb74ec288a
Mark Fynes <tfynes@pivotal.io>
2021-07-21 17:37:21 -0400

    first commit

---
+++ pack.sh
@@ -0,0 +1,6 @@
+kp image create hellopython --tag <targetRegistry> --local-path . -w
+
+echo 
+echo k run hellopython --image=<targetRegistry> --restart=Never --env="PORT=8080"
+echo k expose pod hellopython --port=80 --target-port=8080 --type=LoadBalancer
+echo k get svc | grep 
