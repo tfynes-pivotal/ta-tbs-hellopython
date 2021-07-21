3fe5230386001b368d0be3900abc856d05a0d0b7
Mark Fynes <tfynes@pivotal.io>
2021-07-21 17:47:38 -0400

    pack.sh +x

--- pack.sh
+++ pack.sh
@@ -3,4 +3,4 @@ kp image create hellopython --tag <targetRegistry> --local-path . -w
 echo 
 echo k run hellopython --image=<targetRegistry> --restart=Never --env="PORT=8080"
 echo k expose pod hellopython --port=80 --target-port=8080 --type=LoadBalancer
-echo k get svc | grep 
+echo k get svc 
