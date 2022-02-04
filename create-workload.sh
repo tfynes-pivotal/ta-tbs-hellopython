851e48b5ccf8bff26c50ce11cc0565baf9553608
Mark Fynes <tfynes@vmware.com>
2022-02-04 13:11:34 -0500

    fix to create workload

--- create-workload.sh
+++ create-workload.sh
@@ -1,1 +1,1 @@
-tanzu apps workload create hp4 --git-repo gitRepoPrefix/appName --git-branch main --type web -y
+tanzu apps workload create hp4 --git-repo sourceRepoPrefix/appName --git-branch main --type web -y
