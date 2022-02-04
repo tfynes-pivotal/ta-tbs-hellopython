49adf0f089b8a4e6f37f439b5b166414f8441f6e
Mark Fynes <tfynes@vmware.com>
2022-02-04 12:56:05 -0500

    typo in mirror script

--- mirror-2-github.sh
+++ mirror-2-github.sh
@@ -2,7 +2,7 @@ git init -b main
 git add . 
 git commit -m "..."
 gh repo create appName --public
-git remote add origin https://github.com/sourceRepoPrefix/appName
+git remote add origin sourceRepoPrefix/appName
 git branch -M main
 git push -u origin main
 
