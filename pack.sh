8aebb249a09a6a7aeff31467f00343688545e6ba
Mark Fynes <tfynes@vmware.com>
2022-02-04 12:52:26 -0500

    changed 'hellopython' to 'appName'

--- pack.sh
+++ pack.sh
@@ -1,6 +1,6 @@
-kp image create hellopython --tag targetRegistry --local-path . -w
+kp image create appName --tag targetRegistry --local-path . -w
 
 echo 
-echo kubectl run hellopython --image=targetRegistry --restart=Never --env="PORT=8080"
-echo kubectl expose pod hellopython --port=80 --target-port=8080 --type=LoadBalancer
+echo kubectl run appName --image=targetRegistry --restart=Never --env="PORT=8080"
+echo kubectl expose pod appName --port=80 --target-port=8080 --type=LoadBalancer
 echo kubectl get svc 
